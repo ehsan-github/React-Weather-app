c05a3e995365e0aa13dccb2772b7ea4af317fd4b
ehsanheydariyk <ehsan.heydari.yk@gmail.com>
2017-02-13 13:52:21 +0330

    About page updated

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,5 +1,12 @@
 var React = require('react');
 
-var About = () => <h3>About component</h3>;
+var About = () => {
+  return (
+    <div>
+      <h3>About</h3>
+      <p>Welcome to About page</p>
+    </div>
+  );
+};
 
 module.exports = About;
